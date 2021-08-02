c22f12fbc01560672ee09df981f376dac23c6068
unknown <cristinaandres2001julio@gmail.com>
2021-08-02 01:25:54 -0500

    Finished Gatsby Tutorial  Part 2

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import { Link } from 'gatsby'
+import React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+  return (
+    <Layout pageTitle="About Me">
+      <p>Hi, i'm Cristian a Software Developer, i'm building this site with Gatsby!</p>
+    </Layout>
+  )
+}
+
+export default AboutPage
