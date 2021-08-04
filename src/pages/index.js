1d910ff4d054c2050c8f126a6941c66d43c6b2f2
unknown <cristinaandres2001julio@gmail.com>
2021-08-04 18:10:53 -0500

    Finished Gatsby Tutorial Part 4

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import { Link }from 'gatsby'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
       <p>I'm making this by following the Gatsby tutorial.</p>
+      <StaticImage 
+        // src="https://www.teahub.io/photos/full/35-358921_anime-in-philippines-slam-dunk-slam-dunk-hd.jpg" 
+        src="../images/onizuka.png" 
+        alt="Onizuka with a guitar on his back"
+      />
     </Layout>
-  )
+  );
 }
 
 export default IndexPage
